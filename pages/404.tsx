ec9b6919dd700585e220fe9956d956dd9a96e1b1
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-05-25 02:23:21 -0400

    update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,14 +5,12 @@ export default function PageNotFound(): ReactElement {
   return (
     <div className='bg-gray-300 h-screen'>
       <div className='text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
-        <h1 className='text-2xl'>Uh oh! No Pokemon here</h1>
-        <ul>
-          <li>
-            <Link href='/'>
-              <a>Go back to home to see more Pokemon!</a>
-            </Link>
-          </li>
-        </ul>
+        <h1 className='text-2xl mb-4'>Uh oh! No Pokemon here</h1>
+        <Link href='/'>
+          <a className='p-2 bg-blue-400 rounded-md text-white px-4'>
+            Go back home
+          </a>
+        </Link>
       </div>
     </div>
   )
